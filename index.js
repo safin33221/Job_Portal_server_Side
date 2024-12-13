216583ad44199b12a14c2bba514b24523ed68ba3
safin33221 <saifn33221@gmail.com>
2024-12-13 14:49:06 +0600

    post and get my apllication data

--- index.js
+++ index.js
@@ -10,13 +10,11 @@ app.use(express.json())
 
 
 
-//JobPortal
-//oiHyjRZmEUAcLgYM
 
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.blz8y.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -32,7 +30,54 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         // await client.connect();
-        
+
+        //jobs collection 
+        const jobCollection = client.db('JobPortal').collection('jobs')
+        const jobApplicationCollection = client.db('JobPortal').collection('job_application')
+
+        app.get('/jobs', async (req, res) => {
+            const cursor = jobCollection.find()
+            const result = await cursor.toArray()
+            res.send(result)
+
+        })
+
+        app.get('/jobs/:id', async (req, res) => {
+            const id = req.params.id
+            const filter = { _id: new ObjectId(id) }
+            const result = await jobCollection.findOne(filter)
+            res.send(result)
+        })
+
+
+
+
+        app.get('/job-application', async (req, res) => {
+            const email = req.query.email
+            const filter = { applicant: email }
+            const result = await jobApplicationCollection.find(filter).toArray()
+
+            for (const item of result) {
+                
+                const query1 = { _id: new ObjectId(item.job_Id) }
+                const job = await jobCollection.findOne(query1)
+                if (job) {
+                    item.title = job.title;
+                    item.company = job.company;
+                    item.company_logo = job.company_logo;
+                    item.location = job.location;
+                    item.category = job.category;
+                    item.applicationDeadline = job.applicationDeadline;
+                }
+            }
+            res.send(result)
+        })
+        app.post('/job-application', async (req, res) => {
+            const application = req.body
+            const result = await jobApplicationCollection.insertOne(application)
+            res.send(result)
+        })
+
 
 
 
